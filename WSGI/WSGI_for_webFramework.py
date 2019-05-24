0a3fb30256f96b5aadf8db732bd9052a4d548f97
CAMERON_PATRICK_GUTHRIE <CAMPATGUTHRIE@GMAIL.COM>
2019-05-24 17:15:55 -0500

    Fixed spelling errors

--- WSGI/WSGI_for_webFramework.py
+++ WSGI/WSGI_for_webFramework.py
@@ -18,15 +18,16 @@ class WSGIServer(object):
 			self.socket_type
 		)
 		# Allow to reuse the same address
-		listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDRESS, 1)
+		listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		# Bind
-		listen_socket.bind(server_adress)
+		listen_socket.bind(server_address)
 		# Activate
 		listen_socket.listen(self.request_queue_size)
 		# Get server host name and port
 		host, port = self.listen_socket.getsockname()[:2]
 		self.server_name = socket.getfqdn(host)
-		self.server_port = port# Return headers set by Web framework/Web application
+		self.server_port = port
+		# Return headers set by Web framework/Web application
 		self.headers_set = []
 
 	def set_app(self, application):
@@ -42,7 +43,7 @@ class WSGIServer(object):
 			self.handle_one_request()
 
 	def handle_one_request(self):
-		self.request_data = request_data = self.slient_connection.recv(1024)
+		self.request_data = request_data = self.client_connection.recv(1024)
 		# Print formatted request data a la 'curl -v'
 		print(''.join(
 			'< {line}\n'.format(line=line)
@@ -77,21 +78,21 @@ class WSGIServer(object):
 		# to emphasize the required variables and their balues
 		#
 		# Required WSGI variables
-		env['wsgi.version']       = (1,0)
+		env['wsgi.version']       = (1, 0)
 		env['wsgi.url_scheme']    = 'http'
-		env['wsgi.input']		  = StrinIO.StringIO(self.request_data)
+		env['wsgi.input']		  = StringIO.StringIO(self.request_data)
 		env['wsgi.errors']        = sys.stderr
 		env['wsgi.multithread']   = False
 		env['wsgi.multiprocess']  = False
 		env['wsgi.run_once']      = False
-		# Required CGI variables 
+		# Required CGI variables
 		env['REQUEST_METHOD']     = self.request_method    # GET
 		env['PATH_INFO']          = self.path              # /hello
 		end['SERVER_NAME']        = self.server_name       # localhost
 		env['SERVER_PORT']        = str(self.server_port)  # 8888
 		return env
 
-	def Start_response(self, status, response_headers, exc_info=None):
+	def start_response(self, status, response_headers, exc_info=None):
 		# Add necessary server headers
 		server_headers = [
 			('Date', 'Tue, 31 Mar 2015 12:54:48 GMT'),
@@ -111,7 +112,7 @@ class WSGIServer(object):
 				response += '{0}: {1}\r\n'.format(*header)
 				response += '\r\n'
 			for data in result:
-				response += data 
+				response += data
     		# Print romatted response data a la 'curl -v'
 			print(''.join(
     			'> {line}\n'.format(line=line)
@@ -134,10 +135,10 @@ def make_server(server_address, application):
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		sys.exit('Provide a WSGI application object as module:callable')
-		app_path = sys.argv[1]
-		module, application = app_path.split(':')
-		module = __import__(module)
-		application = getattr(module, application)
-		httpd = make_server(SERVER_ADDRESS, application)
-		print('WSGIServer: Serving HTTP on port {port} ...\n'.format(port=PORT))
-		httpd.serve_forever()
+	app_path = sys.argv[1]
+	module, application = app_path.split(':')
+	module = __import__(module)
+	application = getattr(module, application)
+	httpd = make_server(SERVER_ADDRESS, application)
+	print('WSGIServer: Serving HTTP on port {port} ...\n'.format(port=PORT))
+	httpd.serve_forever()
